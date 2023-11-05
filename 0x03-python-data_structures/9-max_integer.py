067ba0e0f1634e97c2c047ff97e5a97c6a36df56
Bruka25 <bkteshome862@gmail.com>
2023-11-05 11:40:22 -0800

    find the maximum value from the list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+
+    if not my_list:
+        return None
+
+    max_num = my_list[0]
+    # traverse through the list to find the biggest integer
+    for x in my_list:
+        if x > max_num:
+            max_num = x
+    return max_num
