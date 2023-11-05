16ec7b283a7a3d2101f1fc4e896f7e33aa64def2
Bruka25 <bkteshome862@gmail.com>
2023-11-05 12:10:10 -0800

    return numbers divisible by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list):
+    div_list = my_list[:]
+
+    idx = 0
+    for x in new_list:
+        if x % 2 == 0:
+            div_list[index] = True
+        else:
+            div_list[index] = False
+        idx += 1
+
+    return (div_list)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
 
-    if not my_list:
-        return None
+   if not my_list:
+      return None
 
-    max_num = my_list[0]
-    # traverse through the list to find the biggest integer
-    for x in my_list:
-        if x > max_num:
-            max_num = x
-    return max_num
+   max_num = my_list[0]
+   #traverse through the list to find the biggest integer
+   for x in my_list:
+       if x > max_num:
+           max_num = x
+   
+   return max_num
