35b8a0e7d4967d270027e448707d8189b561934a
Bruka25 <bkteshome862@gmail.com>
2023-11-05 12:22:15 -0800

    divisible by 2 second trial

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
-def divisible_by_2(my_list):
-    div_list = my_list[:]
+def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return
 
-    idx = 0
-    for x in new_list:
+    div_list = []
+    for x in my_list:
         if x % 2 == 0:
-            div_list[index] = True
+            div_list.append(True)
         else:
-            div_list[index] = False
-        idx += 1
-
-    return (div_list)
+            div_list.append(False)
+    return div_list
