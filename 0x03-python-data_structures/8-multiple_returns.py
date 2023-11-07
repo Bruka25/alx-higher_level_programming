1f649aa482f076ded7b631b2134c3d2e45c23570
Bruka25 <bkteshome862@gmail.com>
2023-11-07 07:34:29 -0800

    maximum integer pycodestyle fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
+   length = len(sentence)
 
 # if sentence is empty return None
-    if not sentence:
-        return (0, None)
+   if not sentence:
+      return (0, None)
 
-    first = sentence[0]
-    return length, first
+   first = sentence[0]
+      return length, first
