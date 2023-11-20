3f7cec4d921910c9e322befdaa534ca505251c8c
Bruka25 <bkteshome862@gmail.com>
2023-11-21 00:01:32 +0300

    add tasks from fifth to seventh to the repo

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# this function raises exception
+
+
+def raise_exception():
+
+    raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+
+    # this function raises nameerror with message
+    raise NameError(message)
