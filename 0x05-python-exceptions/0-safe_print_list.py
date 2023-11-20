ba5d78e5d07d90b411d8308336c8622b683b4aa8
Bruka25 <bkteshome862@gmail.com>
2023-11-20 22:27:00 +0300

    print the python list safely

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    index = 0
+
+    try:
+        for j in my_list:
+            if idx < x:
+                print('{}'.format(my_list[index]), end='')
+                index += 1
+
+        print()
+    except TypeError:
+        pass
+    finally:
+        return index
