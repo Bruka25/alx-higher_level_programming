2b76fffa3c55921a64a0e712ee72cfe47b14c47d
Bruka25 <bkteshome862@gmail.com>
2023-11-21 00:30:51 +0300

    magic calculation another trial for bare exeption error

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    """
-    magic calculation based on
-    a given bytecode
-    """
 
     output = 0
 
     for num in range(1, 3):
         try:
             if num > a:
-                raise Exception('Too far')
-            else:
-                output += a ** b / num
+                raise Exception("Too far")
+            output += (a ** b) / num
         except:
             output = b + a
             break
 
-    return (output)
+    return output
