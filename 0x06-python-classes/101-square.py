f318b87db88b151391cea151309c95aaca88bb7e
Bruka25 <bkteshome862@gmail.com>
2023-11-22 00:11:31 +0300

    101 square pep8 fix

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
 
- """Class that defines a square by size, which defaults 0
-       if size is equal to 0 print an empty line
-       position must be a tuple of 2 positive integers
-       position should be use by using space 
-       lines should not be filled by spaces 
-       when position[1] > 0
-       Getters and setters for private instance attribute size
-       def my_print(self): that prints in stdout the square with 
-       the character #
+"""
+Another square class based on the previous tasks
 
-    """
+Size is private instance attribute
+Error will be raised on invalid inputs
+def area(self): returns size of area
+Getters and setters for private instance attribute size
+def my_print(self): that prints in stdout the square with the character #
+"""
+
 
 class Square:
-
-    """
-    Class that defines a square by size, which defaults 0
-       if size is equal to 0 print an empty line
-       position must be a tuple of 2 positive integers
-       position should be use by using space
-       lines should not be filled by spaces
-       when position[1] > 0
-    """
-
-    class Square:
 
     """
     Class that defines a square by size, which defaults 0
@@ -44,7 +32,6 @@ class Square:
 
     @size.setter
     def size(self, size):
-
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -57,7 +44,6 @@ class Square:
 
     @position.setter
     def position(self, value):
-
         if type(value) != tuple or len(value) != 2 or \
            not all([type(i) == int for i in value]):
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -70,7 +56,6 @@ class Square:
         return self.__size * self.__size
 
     def get_str(self):
-
         count = ""
         if self.__size is 0:
             count += "\n"
@@ -86,5 +71,3 @@ class Square:
 
     def my_print(self):
         print(self.get_str())
-
-
