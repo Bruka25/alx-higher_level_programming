e320e0d7aaf2474191ba464edf77df9b123127d8
Bruka25 <bkteshome862@gmail.com>
2023-11-21 19:34:13 +0300

    a square class that computes area with and prins it with #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+Another square class based on the previous tasks
+
+Size is private instance attribute
+Error will be raised on invalid inputs
+def area(self): returns size of area
+Getters and setters for private instance attribute size
+
+"""
+
+
+class Square:
+
+    """Class that defines a square by size
+       def my_print(self): that prints in stdout 
+       the square with the character #
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size is 0:
+            print("")
+        for i in range(self.__size):
+            print("#" * self.__size)
