fe6fd92e1c4f88136845366acbaa3b5b75977e81
Bruka25 <bkteshome862@gmail.com>
2023-12-04 23:56:15 +0300

    7 base geometry test documentation fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,11 +20,14 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter with args name (str)
-           which is the name of the parameter and value
-           (int) which is the parameter to validate
-           Raises a TypeError if value is not an integer
-           or ValueError if value is <= 0.
+        """Validate a parameter as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
         """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
