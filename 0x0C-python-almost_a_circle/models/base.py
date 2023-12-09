62296f6b5d3a056e15e4ba28627c547085d74ba4
Bruka25 <bkteshome862@gmail.com>
2023-12-09 21:33:22 +0300

    update base class with decoding the json reprsentaton

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -58,3 +58,13 @@ class Base:
                 attr_json.append(elements.to_dictionary())
 
             return file.write(cls.to_json_string(attr_json))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Static function that returns list of json string
+           representation json_string
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+
+        return json.loads(json_string)
