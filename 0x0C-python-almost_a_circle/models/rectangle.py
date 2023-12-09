e8e51612162896bde5139e4b53774c5aabda3d67
Bruka25 <bkteshome862@gmail.com>
2023-12-09 20:27:09 +0300

    update rectangle class with dictionary representation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -167,3 +167,15 @@ class Rectangle(Base):
                     self.x = val
                 elif key == "y":
                     self.y = val
+
+    def to_dictionary(self):
+        """Function that returns the dictionary representation
+           of a Rectangle
+        """
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+            }
