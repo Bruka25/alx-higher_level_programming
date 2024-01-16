5d0f16ff5122df809c569d03ac74ab95c96a3d6e
Bruka25 <bkteshome862@gmail.com>
2024-01-16 11:14:28 -0800

    list the name and the score on descending order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Script that lists all the tables of second_table
+-- Dsiplays both the score and the name
+-- Order them in descending order
+
+SELECT score, name FROM second_table
+ORDER BY score DESC;
