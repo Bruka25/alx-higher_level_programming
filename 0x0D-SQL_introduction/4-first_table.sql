5ffd77c56a9a4ebad64eb8d975e0e35b37116ad1
Bruka25 <bkteshome862@gmail.com>
2024-01-16 10:24:58 -0800

    create a table first_table on the mysql server

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Script that creates a table called first_table
+-- With the  discription of 'id' INT
+-- and 'name' VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
