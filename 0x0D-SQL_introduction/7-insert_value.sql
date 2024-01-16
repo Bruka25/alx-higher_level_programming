571b9291ccfba8e70d18b46cc89eb16730548acf
Bruka25 <bkteshome862@gmail.com>
2024-01-16 10:50:08 -0800

    insert a new row to the database

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Script that inserts a new row into the table first_table
+-- With values of id = 89
+-- And name = 'Best School'
+
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
