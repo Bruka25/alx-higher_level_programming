1e081690c2030349623dd5f288ab55bc1e34b042
Bruka25 <bkteshome862@gmail.com>
2024-01-16 11:54:18 -0800

    calculate the average of the second table

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
---- Script that calculate the average score of the second_table
-SELECT AVG('score') AS 'average' FROM second_table;
+-- Script that calculate the average score of the second_table
+SELECT AVG('score') AS 'average' FROM 'second_table';
