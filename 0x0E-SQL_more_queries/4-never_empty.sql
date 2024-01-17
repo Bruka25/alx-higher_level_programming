368036924f41d6038980091b055fa2cf5c0da0dd
Bruka25 <bkteshome862@gmail.com>
2024-01-17 12:50:56 -0800

    sql script in which the id cant be null

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- Script that creates table 'id_not_null'
+-- id INT default value 1, name VARCHAR(256)
+-- Script should run if teh table exists already
+
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
