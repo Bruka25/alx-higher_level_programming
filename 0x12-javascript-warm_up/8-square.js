f4ff129d4bab83519d35d9145120b80357277564
Bruka25 <bkteshome862@gmail.com>
2024-02-13 00:13:23 +0300

    javascript for printing square with x

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const square = Math.floor(Number(process.argv[2]));
+if (isNaN(square)) {
+  console.log('Missing size');
+} else {
+  for (let r = 0; r < square; r++) {
+    let row = '';
+    for (let c = 0; c < square; c++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
