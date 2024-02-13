d4f240adf8c4545383a0339b6b774910943e519a
Bruka25 <bkteshome862@gmail.com>
2024-02-13 23:49:06 +0300

    javascript square class that with method charPrint to print a rectangle with character c

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// Javascript square class that inherits from rectangle
+// With method charPrint to print 'C' or 'X'
+
+const Rectangle = require('./5-square');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += c;
+        }
+        console.log(row);
+      }
+    }
+  }
+};
