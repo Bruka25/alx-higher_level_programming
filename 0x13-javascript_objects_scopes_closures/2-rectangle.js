0e315b525d8a5432a2d967531cfbf2bcf3d15c98
Bruka25 <bkteshome862@gmail.com>
2024-02-13 20:49:27 +0300

    javascript that creates an object if the parameters of the rectangle are both positive

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Javascript that checks if the arguments of
+// of the rectangle are positive integers
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+};
