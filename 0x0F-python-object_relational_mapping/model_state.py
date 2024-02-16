e024b937e7cbfbbd0a95df06c2fc205d8e8b3d6c
Bruka25 <bkteshome862@gmail.com>
2024-02-16 15:06:39 -0800

    python script that links to the database by usiing the instance declarative base

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""Python class State, that inherits from
+   state
 """
-import sys
-from sqlalchemy import create_engine, Column, Integer, String
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
+    """ Represents a state for a MySQL database
+
+        __tablename__ (str): The name of the MySQL table to store states
+        id: The state's id.
+        name: The state's name
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128))
-
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
