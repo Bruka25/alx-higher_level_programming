5a2d8121eebadb1a054d7ba9cf217b34c3cb244f
Bruka25 <bkteshome862@gmail.com>
2024-02-19 15:54:43 +0300

    100 relationship city class documentation fix

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -10,6 +10,16 @@ Base = declarative_base()
 
 
 class City(Base):
+    """ Class city that links to mysql table cities
+
+        __tablename__: the name of the table
+        id: primary key that represents the column
+            of auto-generated unique integers
+        name: represents a column of a string of 128
+              characters and can't be null
+        state_id: foreign key to states id and can't be
+                  null
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True,
                 nullable=False, autoincrement=True)
