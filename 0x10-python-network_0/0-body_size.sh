3efceec29a0daf2c8c85eeb24a170f6e76890a7e
Bruka25 <bkteshome862@gmail.com>
2024-02-29 22:50:20 +0300

    add README and first task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# bash script hat takes in a URL, sends a request to that URL,
-#+ and sends the size of the body response
+# bash script hat takes in a URL, sends a request to that URL, and sends the size of the body response
 curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
