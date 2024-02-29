5e0da8201e2c0b9eec78e38dc5e1d9c86c54b390
Bruka25 <bkteshome862@gmail.com>
2024-03-01 02:18:04 +0300

    Bash script to send POST request with json file as the second argument

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that uses curl to send a POST request to the specified URL ($1)
-curl -s -H "Content-Type: bruka/json" -d "$(cat "$2")" "$1"
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
