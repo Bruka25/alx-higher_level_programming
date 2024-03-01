4e83cf46c7422d0a0d2910aed6e6ddf8fc8ee17c
Bruka25 <bkteshome862@gmail.com>
2024-03-02 01:49:51 +0300

    python script that sends post request with a letter as a parameter

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python scirpt that sends POST request with the letter
+   as a parameter
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    res = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        res = req.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get("id"), res.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
