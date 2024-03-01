7cb10c635e1d95aef2da500db0c3437935984057
Bruka25 <bkteshome862@gmail.com>
2024-03-02 02:20:16 +0300

    python script for displaying ten commits from most recent to the oldest

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""python script for printing 10 commits from
+   the most recent to the oldest
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    req = requests.get(url)
+    commits = req.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
